0e475b1dc19e268f523bdab7bafaf356354bec1c
Bishal Khanal <bishal.khanal87@gmail.com>
2022-04-18 16:15:14 -0700

    completed lab1

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,1 +1,34 @@
-# To Do
+print("""**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+""")
+
+appetizers = ['Appetizers', '----------', 'Wings', 'Cookies', 'Spring Rolls']
+entrees = ['Entrees', '-----', 'Salmon', 'Steak', 'Meat Tornado', 'A Literal Garden']
+desserts = ['Desserts', '--------', 'Ice Cream', 'Cake', 'Pie']
+drinks = ['Drinks', '------', 'Coffee', 'Tea', 'Unicorn Tears']
+
+
+print("\n".join(appetizers))
+print("\n".join(entrees))
+print("\n".join(desserts))
+print("\n".join(drinks))
+
+print("""***************************************
+** What would you like to order? **
+*************************************** """)
+
+items = {}
+
+while True:
+    order = input('> ')
+    if order == 'quit':
+        break
+    elif order not in items:
+        items[order] = 0
+
+    items[order] += 1
+    print(f"** {items[order]} order of {order} have been added to your meal **")
